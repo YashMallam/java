24f9cc36bed4cf440922d98620448b2276ba751d
YashMallam <116734650+YashMallam@users.noreply.github.com>
2023-11-22 16:50:59 +0530

    Add files via upload

---
+++ Numpat3.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Numpat3 {
+
+	public static void main(String[] args) {
+		for (int r=1;r<=9;r++) {
+			for (int c=1,v=r;c<=9;c++,v++) {
+				if(v>9) v=1;
+				System.out.print(v+" ");	
+			}
+			System.out.println();
+		}
+
+	}
+
+}
