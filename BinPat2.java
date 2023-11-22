410fce781c66fc4eb223fcccaf1bd9c29bd639e0
YashMallam <116734650+YashMallam@users.noreply.github.com>
2023-11-22 16:12:03 +0530

    Add files via upload

--- BinPat2.java
+++ BinPat2.java
@@ -5,7 +5,7 @@ public class BinPat2 {
 	public static void main(String[] args) {
 		for(int r=1;r<=5;r++) {
 			for( int c=1,t;c<=r;c++) {
-				if(c%2==0) t=1;
+				if((r+c)%2==0) t=1;
 				else t=0;
 				System.out.print(t+" ");
 			}
