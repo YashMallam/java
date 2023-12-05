55cfc7f28fc78e1be593b2efff620d8dd64baec4
YashMallam <116734650+YashMallam@users.noreply.github.com>
2023-12-05 15:46:59 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		int[] num= {20,23,36,78,84,56,98};
+		System.out.println("total elements--"+num.length);
+		for(int i=0;i<num.length;i++) {
+			System.out.println("num["+i+"]---"+num[i]);
+		}
+
+	}
+
+}
