97806f5c5555cd3e09e7b988ef775abc81c9e522
YashMallam <116734650+YashMallam@users.noreply.github.com>
2024-01-03 08:40:08 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+		int option=4;
+		
+		switch (option) {
+		
+		case 1:
+			System.out.println("print 1");
+			break;
+		case 2:
+			System.out.println("print 2");
+			break;
+		case 3:
+			System.out.println("print 3");
+			break;
+		default:
+			System.out.println("print default");
+			
+		}
+
+	}
+
+}
